61aa3fe344ae04556c421de3bca301d109138113
sentamurobinjohnson-cyber <sentamurobinjohnson@gmail.com>
2026-03-04 11:37:34 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1686,13 +1686,13 @@ app.post("/api/inventory/movements", auth, async (req, res) => {
 // -----------------------------
 initDb()
   .then(() => {
-    app.listen(PORT, "0.0.0.0", () => console.log(`✅ Server running on port ${PORT}`));
+    app.listen(PORT, "0.0.0.0", () => {console.log(`✅ Server running on port ${PORT}`));
   })
   .catch((e) => {
     console.error("❌ Failed to init DB:", e);
     process.exit(1);
-
   });
+
 
 
 
